aa1809f2d4aad05a1bb4debb616959acda366717
Konrad Podrygalski <75859974+kpodrygalski@users.noreply.github.com>
2022-08-02 19:23:23 +0200

    Initialized base iOS app structure

---
+++ BMICalculator/BMICalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BMICalculatorApp.swift
+//  BMICalculator
+//
+//  Created by Konrad Podrygalski on 02/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct BMICalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BMICalculator/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  BMICalculator
+//
+//  Created by Konrad Podrygalski on 02/08/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
